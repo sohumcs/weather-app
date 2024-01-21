6ac00add09eeb130a783f63466f4acadb6bb8b05
Sohum Chandra Srivastava <sohum611@gmail.com>
2024-01-21 19:26:51 +0530

    commit

--- main.js
+++ main.js
@@ -158,7 +158,7 @@ function fetchWeatherData() {
     })
 
     .catch(() => {
-      alert("City not found, please try again");
+      alert("City not found, please try agai");
       app.style.opacity = "1";
     });
 }
